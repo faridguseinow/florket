911329dca608aa46cf1cec852a190e4c15a61087
Farid <farid.guseinow@gmail.com>
2026-04-12 16:24:59 +0400

    Fix Vercel path alias and add subcategory showcase config

---
+++ src/constants/subcategoryShowcase.js
@@ -0,0 +1,65 @@
+export const SUBCATEGORY_SHOWCASE = [
+  {
+    key: "all",
+    label: "Все букеты",
+    enabled: true,
+    image:
+      "https://sun9-15.userapi.com/s/v1/ig2/MC8QfDXKqu9IMLcc3Jd1Evv3HOFqmKUzZe8OmRE7rjYBSc9RvL0qe3IyVfjyHSQxPV4s7xP9-j_L4Rf7fysvTcNX.jpg?quality=95&as=32x19,48x29,72x43,108x65,160x96,240x144,360x216,480x288,500x300&from=bu&cs=500x0",
+  },
+  {
+    key: "Розы",
+    label: "Розы",
+    enabled: true,
+    image:
+      "https://sun9-19.userapi.com/s/v1/ig2/aRFp5jjolVD3v1rKYBF3okFc-UApubQe_GNqmHiip7n6K0k2e4Nvw6rLMIaT12i2n3K_L-JKwWFIybUOn3aAXaJi.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1080x1080,1100x1100&from=bu&cs=1100x0 ",
+  },
+  {
+    key: "Тюльпаны",
+    label: "Тюльпаны",
+    enabled: true,
+    image:
+      "https://sun9-40.userapi.com/s/v1/ig2/c8xTkjyTfYqDHL8JDqi6WkH5HPLaQ6I-6vS6jPu4n1LEZGCwVnZGYeyDDzQlSc7oEjkCMfUxO-u4vZuBzqvEBlmY.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,600x600&from=bu&cs=600x0",
+  },
+  {
+    key: "Пионы",
+    label: "Пионы",
+    enabled: true,
+    image:
+      "https://sun9-58.userapi.com/s/v1/ig2/t5FH19H_dzKG2ASCbki0k4esVPPZ_PjsddVdOi3abUalTCQb7gzhwISaM1CAygxxyGun6FPn5zHSuK48DoskmNaD.jpg?quality=95&as=32x23,48x34,72x52,108x77,160x115,240x172,360x258,480x344,540x387,612x439&from=bu&cs=612x0",
+  },
+  {
+    key: "Хризантема",
+    label: "Хризантема",
+    enabled: true,
+    image:
+      "https://sun9-65.userapi.com/s/v1/ig2/t8FshVxrcBIoz20UUEpPIrUfhxsCCxO3PoWScSTVW-9XHqJY4UTPLjk5rGZJtAXQ0RkCVRAqT0Nu7K_e70FYMt2e.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,536x536&from=bu&cs=536x0",
+  },
+  {
+    key: "Гортензия",
+    label: "Гортензия",
+    enabled: true,
+    image:
+      "https://sun9-43.userapi.com/s/v1/ig2/a1fdO_BIKsTgE4DrWau1dpSaUqbN0M30P0ykYTqjjRQvW6gIlkMr6F-GpeIvjjtCu5yogqoQoiRtiP1n90wV15OK.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,650x650&from=bu&cs=650x0",
+  },
+  {
+    key: "Альстромерия",
+    label: "Альстромерия",
+    enabled: false,
+    image:
+      "https://sun9-9.userapi.com/s/v1/ig2/sAxN1XlHEwOdAdkYrbOzv43NueXcRu7O9DZStIaMg6LG9wpub2QH0pTd-220CbZ9UGRoA1iMpp66T9wSo5Fk3ODk.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,800x800&from=bu&cs=800x0",
+  },
+  {
+    key: "Гербера",
+    label: "Гербера",
+    enabled: false,
+    image:
+      "https://sun9-71.userapi.com/s/v1/ig2/cPhobHCO3m93j78rlSbKSAFl8zBzvdSiuVIcLSoV1JuMb2XOjUOTChxBGVMzmi3MLPkdDeDOD_rvASe7Ir9GRL5n.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1080x1080,1280x1280,1440x1440,2000x2000&from=bu&cs=2000x0",
+  },
+  {
+    key: "Гипсофила",
+    label: "Гипсофила",
+    enabled: false,
+    image:
+      "https://sun9-86.userapi.com/s/v1/ig2/E5Rj3HT1DDuc7p1zCKBJBZGhf9KAsnym4Li1f2nJ_AbpK60BdVlFs4BwrWa2WLIY4mwNSYgnAT4MzEDKST2x8cJE.jpg?quality=95&as=32x21,48x32,72x48,108x72,160x107,240x160,360x240,480x320,540x360,640x427,720x480,970x647&from=bu&cs=970x0",
+  },
+]
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
   resolve: {
     alias: {
-      '@/': '/src/',
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   assetsInclude: ['**/*.glb'],
